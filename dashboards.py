76b981cd90cfd182352d639a8d7f48488b66d84b
Felipe <felipeselemeribeiro@gmail.com>
2024-02-08 17:51:45 -0300

    updt 0802

--- dashboards.py
+++ dashboards.py
@@ -4,11 +4,11 @@ import plotly.express as px
 
 st.set_page_config(layout="wide")
 
-# Com uma visão mensal
-#faturamento por unidade… 
-# tipo de produto mais vendido, contribuição por filial,
-#Desempenho das forma de pagamento…
-#Como estão as avaliações das filiais?
+# Com uma visão mensal:
+# Faturamento por unidade… 
+# Tipo de produto mais vendido, contribuição por filial,
+# Desempenho das forma de pagamento…
+# Como estão as avaliações das filiais?
 
 df = pd.read_csv("supermarket_sales.csv", sep=";", decimal=",")
 df["Date"] = pd.to_datetime(df["Date"])
